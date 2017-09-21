8b4f15145dec408351652ba0b53d392627e9e403
dada8397 <vincent8397@gmail.com>
2017-09-21 13:29:32 +0800

    replace_*

--- util/preprocess.py
+++ util/preprocess.py
@@ -56,4 +56,53 @@ def remove_stopwords(tr):
             data['Text'] = pattern.sub("", data['Text'])
     return None
 
+def replace_target_gene(tr):
+    gene_alias_dict = pickle.load(open('./gene_alias.regex.pkl', 'r'))
+    for item in tr:
+        text = item['text']
+        if not re.match('ID', text):
+            gene = text.split(',')[1]
+            if gene in gene_alias_dict:
+                text = re.sub(gene_alias_dict[gene], '$_TARGET_GENE_$', text)
+        item['text'] = text
+
+def convertVariation(raw_text, variation, alternate_string):
+    amino_acid_dict = pickle.load(open('./amino_acid_dict.pkl', 'r'))
+    variation_info = re.findall('^([A-Za-z])(\d+)([A-Za-z])$', variation)
+
+    if variation_info:
+        original_amino, position, final_amino = variation_info[0]
+
+        variation_list = []
+        for o_amino in amino_acid_dict[original_amino]:
+            for f_amino in amino_acid_dict[final_amino]:
+                variation_list.append(o_amino + position + f_amino)
+
+        variation = '(%s)' % '|'.join(variation_list)
+
+    else:
+        variation = re.escape(variation)
+
+    return re.sub(variation, alternate_string, raw_text)
+
+def replace_target_variant(tr):
+    for item in tr:
+        text = item['text']
+        if not re.match('ID', text):
+            variation = text.split(',')[3]
+            text = convertVariation(text, variation, '$_TARGET_VARIATION_$')
+        item['text'] = text
+
+def replace_classified_variant(tr):
+    answer_dict = pickle.load(open('./answer_dict.pkl', 'r'))
+    for item in tr:
+        text = item['text']
+        if not re.match('ID', text):
+            gene, variation = variant.split(',')[1:3]
+            if gene in answer_dict:
+                for variation in answer_dict[gene]:
+                    answer = answer_dict[gene][variation]
+                    text = convertVariation(text, variation, answer)
+        item['text'] = text
+
 # vi:et:sw=4:ts=4
