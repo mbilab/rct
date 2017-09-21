aeff8ec9bd773150135d79b03d38160774d4c824
bobo <almightybobo11235@gmail.com>
2017-09-21 13:52:21 +0800

    add tfidf function

--- util/encode.py
+++ util/encode.py
@@ -1,20 +1,38 @@
 import os
+import numpy as np
 
 from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 from gensim.models import Doc2Vec
 from gensim.models.doc2vec import LabeledSentence
-from gensim import utils
 
 from keras.preprocessing.text import text_to_word_sequence
 
-def tfidf_SVD(df_data, l):
+
+def use_tfidf(data):
+    sentences = [el['text'] for el in data]
     vect = TfidfVectorizer()
-    sentence_vectors = vect.fit_transform(df_data['Text'])
+    X = vect.fit_transform(sentences)
+    X = [list(filter(lambda a: a != 0, line)) for line in X.toarray()]
+    max_len = np.max([len(a) for a in X])
+    X = [np.pad(a, (0, max_len - len(a)), 'constant', constant_values=0) for a in X]
+    index = 0
+    for _ in data:
+        data[index]['tfidf'] = X[index]
+        index = index + 1
+
+
+def tfidf_SVD(data, l):
+    sentences = [el['text'] for el in data]
+    vect = TfidfVectorizer()
+    sentence_vectors = vect.fit_transform(sentences)
     svd = TruncatedSVD(l)
     sentence_vectors = svd.fit_transform(sentence_vectors)
-    return sentence_vectors
+    index = 0
+    for _ in data:
+        data[index]['tfidf_SVD'] = sentence_vectors[index]
+        index = index + 1
 
 
 def doc2vec(data, l, model):
